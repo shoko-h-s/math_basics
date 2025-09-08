3e1fb8681980fbed9fad0bdbae055f01c79f312b
shoko-h-s <mugwort.biwa@gmail.com>
2025-09-08 20:23:01 +0900

    feat: 因数分解を行う機能を追加

---
+++ parts/factorization.py
@@ -0,0 +1,18 @@
+import sympy
+
+def main():
+    print("\n【因数分解】")
+    user_input = input("因数分解を行う式を入力：")
+
+    try:
+        answer = str(sympy.factor(user_input))
+        print(f"\n計算結果：{answer}")
+
+    except (sympy.SympifyError, NameError):
+        print("入力が数式として無効であるため、正しく演算を行えませんでした。プログラムを終了します。")
+    except Exception as e:
+        print(f"予期せぬエラーが発生しました：{e}")
+        print("プログラムを終了します。")
+
+if __name__ == "__main__":
+    main()
