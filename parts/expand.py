560aecd975ce46ab246fc79584fae617f19eee87
shoko-h-s <mugwort.biwa@gmail.com>
2025-09-08 23:11:31 +0900

    feat: 「1:基礎的な数学計算」と「6:多項式の展開」を統合
    - calculator.py と expand.py の機能が重複している点が多かったので、より高機能だと思われる expand.py に統合。
    - 統合に伴い、expand.py のテキストを修正。
    - calculator.py の削除

--- parts/expand.py
+++ parts/expand.py
@@ -1,11 +1,13 @@
 import sympy
 
 def main():
-    print("\n【多項式の展開】")
-    formula = input("展開する式を入力（ √2 は、sqrt(2) のように表記すること）：")
+    print("\n【基礎的な数学計算】")
+    print("このプログラムでは加減乗除・分数の計算・有理化・多項式の展開が行えます。")
+    print("√2 は、sqrt(2) のように表記してください。")
+    user_input = input("計算式を入力してください：")
 
     try:
-        answer = sympy.expand(formula)
+        answer = sympy.expand(user_input)
         print(f"\n計算結果：{answer}")
 
     except Exception as e:
