90a30fc6f22c1164736d65bc819765e3ef2cb808
shoko-h-s <mugwort.biwa@gmail.com>
2025-09-23 21:27:07 +0900

    feat: 点と直線、平行な2直線の距離を求める機能を追加
    追加に伴い、top_menu.py のメニュー順を一部入れ替えた。

--- parts/distance.py
+++ parts/distance.py
@@ -1,6 +1,6 @@
 import sympy
 
-def main():
+def p2():
     print("\n【2点 A, B 間の距離を求める】")
     a, b = map(int, input("A の x 座標, y 座標を、半角スペース区切りで入力：").split())
     c, d = map(int, input("B の x 座標, y 座標を、半角スペース区切りで入力：").split())
@@ -10,5 +10,36 @@ def main():
 
     print(f"\n計算結果：{answer}")
 
-if __name__ == "__main__":
-    main()
+
+def pl():
+    print("\n点(x0, y0) と、直線ax + by + c = 0 の距離を求める")
+
+    # 入力値をシンボリックな表現で扱う
+    x0, y0 = map(sympy.sympify, input("点の x 座標, y 座標を、半角スペース区切りで入力：").split())
+    a, b, c = map(sympy.sympify, input("a, b, c の値を、半角スペース区切りで入力：").split())
+
+    num1 = abs(a * x0 + b * y0 + c)
+    num2 = sympy.sqrt(a**2 + b**2)
+
+    # 無理数はそのままの形で保持される
+    answer = num1 / num2
+
+    simplified_answer = sympy.simplify(answer)
+
+    print(f"\n計算結果：{simplified_answer}")
+
+    # 浮動小数点数での近似値が必要な場合用
+    # print(f"近似値: {simplified_answer.evalf()}")
+
+
+def ll():
+    print("\n平行な2直線 ax + by + c = 0 と ax + by + d = 0 の距離を求める")
+    a, b, c, d = map(sympy.sympify, input("a, b, c, d の値を、半角スペース区切りで入力：").split())
+
+    num1 = abs(d - c)
+    num2 = sympy.sqrt(a**2 + b**2)
+
+    answer = num1 / num2
+    simplified_answer = sympy.simplify(answer)
+
+    print(f"\n計算結果：{simplified_answer}")
