379a4142a0230c0484a99220fa1a05aea9fd3f4d
shoko-h-s <mugwort.biwa@gmail.com>
2025-09-26 17:19:09 +0900

    feat: 対数方程式を解く機能を parts/expo_equation.py に追加
    方程式の入力時テキストのみ変更すれば、対数方程式を解くのと同じアルゴリズムで対応可能なため、新規ファイルは作らない方針にした。

--- parts/expo_equation.py
+++ parts/expo_equation.py
@@ -1,7 +1,15 @@
 import sympy
 
 def main():
-    print("\n【指数方程式を解く】方程式を左辺, 右辺に分けてそれぞれ入力（入力例：sqrt(2)**x）")
+    op = int(input("\n解く方程式の種類を選択　　1：指数方程式　2：対数方程式： "))
+
+    # 入力メッセージを方程式の種類によって変化させる
+    if op == 1:
+        print("\n【指数方程式を解く】方程式を左辺, 右辺に分けてそれぞれ入力（入力例：sqrt(2)**x）")
+    else:
+        print("\n【対数方程式を解く】方程式を左辺, 右辺に分けてそれぞれ入力")
+        print("対数は sympy.log(真数, 底) の形式で入力（入力例：log 2 4 → sympy.log(4, 2)）")
+
     left_side_str = input("方程式の左辺を入力： ")
     right_side_str = input("方程式の右辺を入力： ")
 
@@ -17,7 +25,8 @@ def main():
         # 実数解のみを抽出
         real_solutions = [sol for sol in all_solutions if sol.is_real]
 
-        print(f"\nx = {real_solutions}")
+        print("\n【解】")
+        print(f"x = {real_solutions}")
 
     except Exception as e:
         print("\n入力された式を解析できませんでした。プログラムを終了します。")
